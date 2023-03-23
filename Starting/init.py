6e6a4c0330b11d8e06b654f2870c9b971b0a6bf6
Darlan <darlaneduardoguimaraes@gmail.com>
2023-03-23 14:42:49 -0300

    start

---
+++ Starting/init.py
@@ -0,0 +1,20 @@
+pais = '10'
+type(pais)
+
+if(pais == 10):
+    print('Aqui')
+else:
+    print('nop')
+
+name1 = 'Nome'
+name2 = 'Sobrenome'
+
+print(name1, name2, sep=" ")
+
+if(pais == 10):
+    if(pais > 9):
+        print('Isso é algo')
+    elif(pais < 9):
+        print('menor')
+    elif(pais > 9):
+        print('algo')
