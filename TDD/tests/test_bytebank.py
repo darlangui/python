0cabf3aed17d9a093ab6f0dba3babf145c85bbe9
Darlan <darlaneduardoguimaraes@gmail.com>
2023-04-08 14:23:47 -0300

    test sobrenome

--- TDD/tests/test_bytebank.py
+++ TDD/tests/test_bytebank.py
@@ -10,3 +10,11 @@ class TestClass:
 
         assert resultado == esperado
 
+    def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self):
+        entrada = 'Lucas Carvalho'
+        esperada = 'Carvalho'
+
+        funcionario_teste = Funcionario(entrada, '12/03/2000', 1111)
+        resultado = funcionario_teste.sobrenome()
+
+        assert resultado == esperada
